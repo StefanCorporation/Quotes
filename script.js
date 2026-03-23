0f94f3e7caa83b1dde1b216880a9897d82aeb51f
Stefan Ashugyan <abrams1234518@gmail.com>
2026-03-23 19:17:37 +0400

    splitted author and quotes

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-const element = document.getElementById('quote');
-const button = document.getElementById('generate-btn');
+const quote = document.getElementById('quote')
+const author = document.getElementById('author')
+const button = document.getElementById('generate-btn')
 
-let quotes = [];
-
+let quotes = []
 
 fetch('./data.json')
-  .then(res => res.json())
-  .then(data => {
-    quotes = data;
-  });
+  .then((res) => res.json())
+  .then((data) => {
+    quotes = data
+  })
 
 function randomQuote() {
-  const random = quotes[Math.floor(Math.random() * quotes.length)];
+  const random = quotes[Math.floor(Math.random() * quotes.length)]
 
-  element.textContent = `"${random.text}" — ${random.author}`;
+  quote.textContent = `"${random.text}"`
+  author.textContent = `${random.author}`
 }
 
-button.addEventListener('click', randomQuote);
-
-
-
+button.addEventListener('click', randomQuote)
 
 //dark Mode
 
-const toggle = document.getElementById('theme-toggle');
+const toggle = document.getElementById('theme-toggle')
 
 toggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark');
-  
-});
+  document.body.classList.toggle('dark')
+})
